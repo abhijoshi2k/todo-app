98970c44da25b8d280c98d1645ee031023a2e44e
Abhishek Nilesh Joshi <abhishek.joshi@perkinelmer.com>
2022-09-20 14:06:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -87,6 +87,6 @@ app.delete('/', async (req, res) => {
 	}
 });
 
-app.listen(3000, () => {
-	console.log('Listening on port 3000!');
+app.listen(process.env.PORT || 3000, () => {
+	console.log('Listening on port ' + (process.env.PORT || 3000));
 });
